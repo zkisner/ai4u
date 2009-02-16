0f08063ec26c48dfa085ec005f94a1b44429d5a8
igal1987 <igal1987@6939737c-85e0-11dd-aace-ddc5e87cd236>
2009-02-16 20:45:22 +0000

    added an extension for java's Random

---
+++ trunk/src/utils/RandomUtils.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * This is an extension for the {@link Random} file.
+ * 
+ * @author kreich
+ */
+public class RandomUtils extends Random {
+
+	/**
+	 * Picks a random element from the given {@link Collection}.
+	 * @param <T> The type of the elements in the {@link Collection}.
+	 * @param collection The {@link Collection} to pick a random element from.
+	 * @return A random element from the {@link Collection}.
+	 */
+	public <T> T pickRandom(Collection<T> collection) {
+		Iterator<T> iter = collection.iterator();
+		for (int i = 0; i < nextInt(collection.size()) - 1; i++)
+			iter.next();
+		return iter.next();
+	}
+	
+	/**
+	 * @param <E> The type of the Enum.
+	 * @param e The enum's class.
+	 * @return A random instance of the Enum.
+	 */
+	@SuppressWarnings("unchecked")
+	public <E extends Enum<E>> E pickRandom(Class<E> e) {
+		try {
+			E[] values = (E[]) e.getMethod("values").invoke(null);
+			return values[nextInt(values.length)];
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
