cb15b41273ecac92b90825e3cfedc5f62239904a
igal1987 <igal1987@6939737c-85e0-11dd-aace-ddc5e87cd236>
2009-06-13 11:18:04 +0000

    moved the util package from the main project to a separate project

---
+++ com.ai4u.util/trunk/src/com/ai4u/util/RandomUtils.java
@@ -0,0 +1,42 @@
+package com.ai4u.util;
+
+import java.util.*;
+
+/**
+ * This is an extension for the {@link Random} file.
+ * 
+ * @author kreich
+ */
+public class RandomUtils extends Random {
+
+	/**
+	 * Picks a random element from the given {@link Collection}.
+	 * @param <T> The type of the elements in the {@link Collection}.
+	 * @param collection The {@link Collection} to pick a random element from.
+	 * @return A random element from the {@link Collection}.
+	 */
+	public <T> T pickRandom(Collection<T> collection) {
+		if (collection.isEmpty()) return null;
+		
+		Iterator<T> iter = collection.iterator();
+		int randIndex = nextInt(collection.size());
+		
+		for (int i = 0; i < randIndex - 1; i++) iter.next();
+		return iter.next();
+	}
+	
+	/**
+	 * @param <E> The type of the Enum.
+	 * @param e The enum's class.
+	 * @return A random instance of the Enum.
+	 */
+	@SuppressWarnings("unchecked")
+	public <E extends Enum<E>> E pickRandom(Class<E> e) {
+		try {
+			E[] values = (E[]) e.getMethod("values").invoke(null);
+			return values[nextInt(values.length)];
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
