a6cc2251bd4b864e906bc871d28a58704adb9e39
igal1987 <igal1987@6939737c-85e0-11dd-aace-ddc5e87cd236>
2010-05-20 09:08:51 +0000

    added a test

--- com.ai4u.games.tictactoe/trunk/tests/com/ai4u/core/logic/computer/MinimaxLogicTest.java
+++ com.ai4u.games.tictactoe/trunk/tests/com/ai4u/core/logic/computer/MinimaxLogicTest.java
@@ -80,4 +80,22 @@ public class MinimaxLogicTest {
 		Assert.assertEquals(new TicTacToeMove(1,1), move);
 	}
 
+	/**
+	 * Checks that o makes a saving move,
+	 *  [x, ,x]
+	 *  [ , , ]
+	 *  [o, , ]
+	 */
+	@Test
+	public void checkOSaves() {
+		TicTacToeBoard board = new TicTacToeBoard(3);
+		board.makeMove(new TicTacToeMove(0,0));
+		board.makeMove(new TicTacToeMove(2,0));
+		board.makeMove(new TicTacToeMove(0,2));
+		
+		Assert.assertFalse(board.isGameOver());
+		TicTacToeMove move = logic.pickMove(board);
+		Assert.assertEquals(new TicTacToeMove(0,1), move);
+	}
+	
 }
